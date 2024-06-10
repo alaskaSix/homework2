ea6b4c6f00a4988117541e27d2cda7cd635b53de
alaskaSix <127185220+alaskaSix@users.noreply.github.com>
2024-06-11 00:28:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+first_name = 'Python'
+completed_homework = 12
+spent_time = 1.5
+time_homework = (spent_time/completed_homework)
+print(first_name,',всего задач:', completed_homework, ',затрачено часов:', spent_time, ', среднее время выполнения',time_homework,'часа.')
